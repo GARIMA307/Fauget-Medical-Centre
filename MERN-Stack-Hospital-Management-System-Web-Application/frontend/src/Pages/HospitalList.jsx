1a09755b84ee43a0a9730461e91621fd1f6d9b7a
Garima Kashyap <garimakashyap2600@gmail.com>
2026-04-01 00:15:28 +0530

    Added validation for feedback form

--- MERN-Stack-Hospital-Management-System-Web-Application/frontend/src/Pages/HospitalList.jsx
+++ MERN-Stack-Hospital-Management-System-Web-Application/frontend/src/Pages/HospitalList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+//Added validation for feedback form
 const HospitalList = () => {
   const [hospitals, setHospitals] = useState([]);
   const navigate = useNavigate();
